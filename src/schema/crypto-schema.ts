6943d8750b94c6deb1fc0287c43a9fe7fc3a9792
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-10-27 09:40:26 -0300

    Validando la respuesta de la API con zod

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -1,6 +1,16 @@
-import { z } from 'zod'
+import { string, z } from 'zod'
 
 export const CurrencySchema = z.object({
     code: z.string(),
     name: z.string()
 })
+
+export const CryptoCurrencyResponseSchema = z.object({
+    CoinInfo: z.object({
+        //anoto solo las propiedades que voy a utilizar
+        FullName: string(),
+        Name: string()
+    })
+})
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)
